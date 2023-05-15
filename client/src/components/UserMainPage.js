65ae88be9e8212c0282d35368823414604777587
Therese Gordon <thereseagordon@gmail.com>
2023-05-15 15:10:32 -0400

    create welcome page with title and links

---
+++ client/src/components/UserMainPage.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const UserMainPage = ({ user }) => {
+    return (
+        <div>
+            <h2>Welcome to your main page</h2>
+            <Link to="/add-a-route" className="mainPageLinks">Add a route</Link>
+            <Link to="/log-your-run" className="mainPageLinks">Log a run</Link>
+            <Link to="/my-routes" className="mainPageLinks">View my saved routes</Link>
+            <Link to="/my-runs" className="mainPageLinks">View my completed runs</Link>
+        </div>
+    )
+}
+
+export default UserMainPage
