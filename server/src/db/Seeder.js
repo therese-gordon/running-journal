2ddce18215f3d4960b30347672a7d7ccd380b871
Therese Gordon <thereseagordon@gmail.com>
2023-05-16 17:16:11 -0400

    create user seeder

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,10 +1,13 @@
 /* eslint-disable no-console */
 import { connection } from "../boot.js"
 import FavoriteRouteSeeder from "./seeders/FavoriteRouteSeeder.js"
+import UserSeeder from "./seeders/UserSeeder.js"
 
 class Seeder {
   static async seed() {
-    // include individual seed commands here
+    console.log("seeding users...")
+    await UserSeeder.seed()
+
     console.log("seeding routes...")
     await FavoriteRouteSeeder.seed()
 
---
+++ server/src/db/seeders/UserSeeder.js
@@ -0,0 +1,26 @@
+import { User } from "../../models/index.js"
+
+class UserSeeder {
+    static async seed() {
+        const userData = [
+            {
+                email: "test1@email.com",
+                password: "123"
+
+            },
+            {
+                email: "test2@email.com",
+                password: "123"
+            }
+        ]
+
+        for (const singleUserData of userData) {
+            const currentUser = await User.query().findOne({ email: singleUserData.email })
+            if (!currentUser) {
+                await User.query().insert(singleUserData)
+            } 
+        }
+    }
+}
+
+export default UserSeeder
