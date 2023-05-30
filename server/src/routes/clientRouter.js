b5de918169f8c993bdc5a778e995c2a58edfde47
Therese Gordon <thereseagordon@gmail.com>
2023-05-29 21:37:43 -0400

    add links to topbar available without login

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -3,7 +3,7 @@ import getClientIndexPath from "../config/getClientIndexPath.js";
 
 const router = new express.Router();
 
-const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/auth/failure", "/profile"];
+const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/auth/failure", "/profile", "/routes", "/runs"];
 
 const authedClientRoutes = ["/profile", "/welcome", "/routes", "/runs", "/add-a-route", "/log-a-run"];
 
